9c20ad842685a54143ec464d80092cd97513148b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-14 01:44:02 +0200

    package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
 
 import express from "express";
+import { analyzeCSS } from "./analyzers/css";
+import { analyzeJS } from "./analyzers/js";
+import { analyzeHTML } from "./analyzers/html";
 
 
 const app = express();
 app.use(express.json());
+
+app.post("/analyze", (req, res) => {
+    const { type, code } = req.body;
+    let results: any[] = [];
+  
+    if (type === "css") results = analyzeCSS(code);
+    if (type === "js") results = analyzeJS(code);
+    if (type === "html") results = analyzeHTML(code);
+  
+    res.json({ results });
+  });
 
 
 
