90d5423969d9e50eaa1bc5bcbf1afef2e5abe40f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-21 21:04:25 +0200

    dev dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { analyzeCSS } = require("./analyzers/css");
-const { analyzeJS } = require("./analyzers/js");
-const { analyzeHTML } = require("./analyzers/html");
 const { analyzeHTML, analyzeCSS, analyzeJS } = require('../../packages/core-analyzer');
 
 
@@ -11,14 +8,13 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/analyze", (req, res) => {
-  console.log("👉 Request body:", req.body);
   const { type, code } = req.body;
   console.log("👉 type:", type, "code:", code);
-  let results = [];
 
+  let results = [];
+  if (type === "html") results = analyzeHTML(code);
   if (type === "css") results = analyzeCSS(code);
   if (type === "js") results = analyzeJS(code);
-  if (type === "html") results = analyzeHTML(code);
 
   res.json({ results });
 });
